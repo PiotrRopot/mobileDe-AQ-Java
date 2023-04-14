ec72af047856a80ae8f6bacf22878097bcb39215
Piotr Ropot <Ropot.Piotr@gmail.com>
2023-04-14 13:41:01 +0300

    add class

--- src/test/java/by/itacademy/piotrropot/mobilede/api/ApiTestMobilede.java
+++ src/test/java/by/itacademy/piotrropot/mobilede/api/ApiTestMobilede.java
@@ -1,40 +1,22 @@
 package by.itacademy.piotrropot.mobilede.api;
 
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import org.json.JSONObject;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-import java.util.Date;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApiTestMobilede {
 
     @Test
-    public void testOpenMobilede() {
-        String URL = "https://www.mobile.de";
+    public void testOpenMobiledeGET() {
+        RestAssured.baseURI = "https://www.mobile.de";
+        String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+                "(KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36";
         Response response = RestAssured.given()
-                .contentType(ContentType.JSON)
-                .get(URL);
-        response.prettyPrint();
+                .header("User-Agent", userAgent)
+                .when()
+                .get();
+        assertEquals(200, response.getStatusCode());
     }
-    @Test
-    public void postRequestExampleTest() {
-        String someRandomString = String.format("%1$TH%1$TM%1$TS", new Date());
-        JSONObject requestBody = new JSONObject();
-        requestBody.put("FirstName", someRandomString);
-        requestBody.put("LastName", someRandomString);
-        requestBody.put("UserName", someRandomString);
-        requestBody.put("Password", someRandomString);
-        requestBody.put("Email", someRandomString + "@gmail.com");
-        RequestSpecification request = RestAssured.given();
-        request.header("Content-Type", "application/json");
-        request.body(requestBody.toString());
-        Response response = request.post("https://webhook.site/a18a23cb-e9a0-4f03-a7fa-cdfcfa76ca98");
-        int statusCode = response.getStatusCode();
-        Assert.assertEquals(statusCode, 200);
-        System.out.println("The status code recieved: " + statusCode);
-}}
+}
